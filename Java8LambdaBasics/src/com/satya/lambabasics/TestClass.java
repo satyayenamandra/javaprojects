23408a4ffd2fa8443cecd93d48fbfd61215b53b7
Dell <Dell@192.168.1.105>
2021-05-13 11:49:17 +0530

    test commit

---
+++ Java8LambdaBasics/src/com/satya/lambabasics/TestClass.java
@@ -0,0 +1,18 @@
+package com.satya.lambabasics;
+
+public class TestClass {
+
+	public TestClass() {
+		// TODO Auto-generated constructor stub
+		
+	
+	}
+	
+	private static void main(String [] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello !");
+		
+	}
+
+}
